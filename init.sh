25f8ee8266d8094d72cb2dc6a3b56db8e2dd705d
Andrew Charneski <acharneski@gmail.com>
2026-06-08 14:19:08 -0400

    play store

---
+++ init.sh
@@ -0,0 +1,4 @@
+export PWA_URL=https://aol.cognotik.com/
+export ANDROID_APP_ID=com.cognotik.arcadeoflife
+npm run build:play:init    # first time only
+npm run build:play:build   # subsequent builds
